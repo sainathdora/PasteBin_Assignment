8b3df7f742c712c8b2f65de720816ce99337762f
Sainath Dora <sainathdora4@gmail.com>
2026-01-29 04:38:25 +0000

    added time based expiry and max-views

--- app/api/pastes/[id]/route.ts
+++ app/api/pastes/[id]/route.ts
@@ -14,53 +14,36 @@ export async function GET(
     if (!res || res.length === 0) {
       return NextResponse.json({ error: "Paste not found" }, { status: 404 });
     }
-    const { Created, Expiry, Content, ViewsLeft } = res[0];
-    console.log(res[0])
+    let { Created, Expiry, Content, ViewsLeft } = res[0];
     const header = req.headers.get('x-test-now-ms');
     const nowMs = (test_on && header) ? Number(header) : Date.now();
     const now = new Date(nowMs);
     console.log("now = ",now)
     console.log("expiry = ", new Date(Expiry));
     console.log(now>new Date(Expiry))
-    if (Expiry && now > new Date(Expiry)) {
-      // expiry not null and now>expiry
-        try{
-          // if expired then delete
-            await delete_paste_by_id(urlId);
-        }catch(err){
-            NextResponse.json({error:"Paste May not exist"}, {status:404});
-        }
+    if(ViewsLeft ==0 || (Expiry && now> new Date(Expiry))){
+      await delete_paste_by_id(urlId);
+      return NextResponse.json({
+        error:"Paste doesnt exist"
+      }, {status:404})
     }else{
-      // (expiry == null) or now<=expiry
-      if(Expiry==null){
-        // worry about ViewsLeft
-        if(ViewsLeft==0){
-          return NextResponse.json({ error: "Paste has expired" }, { status: 404 });
-        }else{
-          // dec viewsLeft by 1
+      if(ViewsLeft!=null){
+        if(ViewsLeft>0){
           try{
-            const updated_paste_views_left = (await decrement_views(urlId))['ViewsLeft'];
-            return NextResponse.json({
-              Content:Content,
-              remaining_views:updated_paste_views_left,
-              expires_at:null              
-            });
+            const views_left_updated = (await decrement_views(urlId))['ViewsLeft'];
+            ViewsLeft = views_left_updated;
           }catch(err){
-            return NextResponse.json({error:"Error Decrementing views"}, {
-              status:500
-            });
+            return NextResponse.json({
+              error:"Failed to decrement viewsLeft"
+            },{status:500})
           }
         }
-      }else{
-        // we have expiry time
-        
       }
     }
-
     return NextResponse.json({
-      content: Content,
-      created: Created,
-      viewsLeft: ViewsLeft
+      Content: Content,
+      remaining_views: ViewsLeft,
+      expires_at: Expiry
     });
 
   } catch (err: any) {
